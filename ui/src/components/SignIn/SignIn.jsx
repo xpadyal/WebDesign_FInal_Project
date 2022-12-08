934e776bb77f7e4e8a878b43a1817b97583c5f6b
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-08 15:41:06 -0500

    created ui with home img

---
+++ ui/src/components/SignIn/SignIn.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import homeImg from "../../img/home.jpg";
+import "./SignIn.css"
+
+
+const SignIn = () => {
+    return (
+        <div className="wrapper">
+          <div className="left">
+            <img src={homeImg} alt="" />
+          </div>
+         
+        </div>
+      );
+}
+
+export default SignIn
