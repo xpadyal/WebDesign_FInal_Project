c5ee888e6a0ac41a89e17fbe2a324a4d91777df0
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-08 16:04:35 -0500

    added login page and updated signin page

--- ui/src/components/LogIn/LogIn.jsx
+++ ui/src/components/LogIn/LogIn.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import homeImg from "../../img/home.jpg";
-import "./SignIn.css";
+import "./LogIn.css";
 
-const SignIn = () => {
+const LogIn = () => {
   return (
     <div className="wrapper">
       <div className="left">
@@ -11,26 +11,18 @@ const SignIn = () => {
       <div className="right">
         <h3 className="title">Sign In</h3>
         <form action="" className="form">
-          <div className="firstName">
-            <label for="fname">First Name</label>
-            <input type="text" id="fname" name="fname" placeholder="john" />
-          </div>
-          <div className="lastName">
-            <label for="lname">Last Name</label>
-            <input type="text" id="lname" name="lname" placeholder="doe" />
-          </div>
           <div className="email">
-            <label for="email">Email</label>
+            <label for="fname">Email</label>
             <input
-              type="email"
-              id="email"
-              name="email"
+              type="text"
+              id="fname"
+              name="fname"
               placeholder="abc@gmail.com"
             />
           </div>
           <div className="password">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="******" />
+            <label for="lname">Password</label>
+            <input type="text" id="lname" name="lname" placeholder="******" />
           </div>
           <input type="submit" value="SIGN IN" className="submit" />
           <div className="links">
@@ -45,4 +37,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default LogIn;
