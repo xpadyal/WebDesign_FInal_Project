dfee4e77fad41c5c6cfcf78b8817df47ff5ee62a
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-08 15:45:40 -0500

    added form to sign in page

--- ui/src/components/SignIn/SignIn.jsx
+++ ui/src/components/SignIn/SignIn.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React from "react";
 import homeImg from "../../img/home.jpg";
-import "./SignIn.css"
-
+import "./SignIn.css";
 
 const SignIn = () => {
-    return (
-        <div className="wrapper">
-          <div className="left">
-            <img src={homeImg} alt="" />
+  return (
+    <div className="wrapper">
+      <div className="left">
+        <img src={homeImg} alt="" />
+      </div>
+      <div className="right">
+        <h3 className="title">Sign In</h3>
+        <form action="" className="form">
+          <div className="email">
+            <label for="fname">Email</label>
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              placeholder="abc@gmail.com"
+            />
           </div>
-         
-        </div>
-      );
-}
+          <div className="password">
+            <label for="lname">Password</label>
+            <input type="text" id="lname" name="lname" placeholder="******" />
+          </div>
+          <input type="submit" value="SIGN IN" className="submit" />
+          <div className="links">
+            <a href="#">Forgot Password</a>
+            <span>
+              Already have an account ? <a href="#">Log In</a>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
