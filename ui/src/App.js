a37b757cd891b814810a394a39681ca9f996b676
Chikateyukta <114361980+Chikateyukta@users.noreply.github.com>
2022-12-09 18:27:02 -0500

    updated routing

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import LogIn from './components/LogIn/LogIn';
-import SignIn from './components/SignIn/SignIn';
-
+import "./App.css";
+import Home from "./components/Home/Home";
+import LogIn from "./components/LogIn/LogIn";
+import SignIn from "./components/SignIn/SignIn";
 
 function App() {
   return (
     <div className="App">
-       <Router>
-          <Routes>
-            <Route path="/" exact element={<SignIn />}></Route>
-            <Route path="/login" element={<LogIn />}></Route>
-          </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/signin" exact element={<SignIn />}></Route>
+          <Route path="/login" element={<LogIn />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
