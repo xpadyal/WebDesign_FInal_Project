e53e18baa030eeb482389df65aa2021b9176ee4f
anirudhajoshi2808 <joshi.anir@northeastern.edu>
2022-12-09 21:56:39 -0500

    SignUp and Login UI

---
+++ src/components/LogIn/LogIn.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import homeImg from "../../img/home.jpg";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+const LogIn = () => {
+  return (
+    <div className="wrapper">
+      <div className="left">
+        <img src={homeImg} alt="" />
+      </div>
+      <div className="right">
+        <h3 className="title">Log In</h3>
+        <form action="" className="form">
+          <div className="email">
+            <label for="fname">Email</label>
+            <input
+              type="text"
+              id="fname"
+              name="fname"
+              placeholder="abc@gmail.com"
+            />
+          </div>
+          <div className="password">
+            <label for="lname">Password</label>
+            <input type="text" id="lname" name="lname" placeholder="******" />
+          </div>
+          <input type="submit" value="LOG IN" className="submit" />
+          <div className="links">
+            <a href="#">Forgot Password</a>
+            <span>
+              Don't have an account ? <Link to="/">Signin</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import homeImg from "../../img/home.jpg";
+import "./SignIn.css";
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <div className="wrapper">
+      <div className="left">
+        <img src={homeImg} alt="" />
+      </div>
+      <div className="right">
+        <h3 className="title">Sign In</h3>
+        <form action="" className="form">
+          <div className="firstName">
+            <label for="fname">First Name</label>
+            <input type="text" id="fname" name="fname" placeholder="john" />
+          </div>
+          <div className="lastName">
+            <label for="lname">Last Name</label>
+            <input type="text" id="lname" name="lname" placeholder="doe" />
+          </div>
+          <div className="email">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="abc@gmail.com"
+            />
+          </div>
+          <div className="password">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="******"
+            />
+          </div>
+          <input type="submit" value="SIGN IN" className="submit" />
+          <div className="links">
+            <span>
+              Already have an account ? <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
