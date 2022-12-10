eff65eeb1de828aa0558bb58667b0363a3603efe
anirudhajoshi2808 <joshi.anir@northeastern.edu>
2022-12-10 00:07:03 -0500

    Sign Up API Integration

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,42 +1,89 @@
-import React from "react";
+import React, { Component } from 'react';
 import homeImg from "../../img/home.jpg";
 import "./SignIn.css";
 import { Link } from "react-router-dom";
 
-const SignIn = () => {
-  return (
-    <div className="wrapper">
+export class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          fname: "",
+          lname: "",
+          email: "",
+          password: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleSubmit(e) {
+        e.preventDefault();
+        const { fname, lname, email, password } = this.state;
+        console.log(fname, lname, email, password);
+        fetch("http://localhost:8000/api/register", {
+          method: "POST",
+          crossDomain: true,
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+          body: JSON.stringify({
+            fname,
+            lname,
+            email,
+            password,
+          }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            window.location.href = '/';
+            console.log(data, "userRegister");
+          });
+      }
+  render() {
+    return (
+      <div className="container">
+        <div className="wrapper">
       <div className="left">
         <img src={homeImg} alt="" />
       </div>
       <div className="right">
         <h3 className="title">Sign Up</h3>
-        <form action="" className="form">
+        <form  onSubmit={this.handleSubmit} className="form">
           <div className="firstName">
-            <label for="fname">First Name</label>
-            <input type="text" id="fname" name="fname" placeholder="john" />
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+            onChange={(e) => this.setState({ fname: e.target.value })}
+          />
           </div>
           <div className="lastName">
-            <label for="lname">Last Name</label>
-            <input type="text" id="lname" name="lname" placeholder="doe" />
+          <label>Last name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+            onChange={(e) => this.setState({ lname: e.target.value })}
+          />
           </div>
           <div className="email">
-            <label for="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="abc@gmail.com"
-            />
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            onChange={(e) => this.setState({ email: e.target.value })}
+          />
           </div>
           <div className="password">
-            <label for="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="******"
-            />
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="*******"
+            onChange={(e) => this.setState({ password: e.target.value })}
+          />
           </div>
           <input type="submit" value="SIGN IN" className="submit" />
           <div className="links">
@@ -47,7 +94,9 @@ const SignIn = () => {
         </form>
       </div>
     </div>
-  );
-};
+      </div>
+    )
+  }
+}
 
-export default SignIn;
+export default SignIn
